33775b666f7b8bac82ca89def8d6670d18a941ae
VictorMunayco96 <m.m.victor9630@gmail.com>
2019-07-08 16:05:49 -0500

    CategoriaDiseño

---
+++ Programa/ProgBalSoft/src/ProgBalSoft/ProgBalSoft.java
@@ -0,0 +1,12 @@
+
+package ProgBalSoft;
+
+import Vista.VCategoria;
+
+
+public class ProgBalSoft {
+    public static void main(String[] args) {
+        VCategoria Obj = new VCategoria();
+        Obj.setVisible(true);
+    }
+}
